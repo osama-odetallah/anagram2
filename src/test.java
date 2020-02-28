836a516543c757be103507157ee94c343e032157
osama odetallah <osamaodetallah@hotmail.com>
2020-02-28 10:08:40 +0200

    adding 3rd string

---
+++ src/test.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+    import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+    public class test {
+        public static void main(String[] args) {
+            String s1="abs";
+            String s2="bsa";
+ String s3="kkk"
+
+            for (int i=0;i<s1.length();i++){
+                Map<Character,Integer> map=new HashMap;
+                if (map.containsKey(s1.charAt(i)))
+
+
+
+
+
+
+            }
+        }
+    }
+
+}
+}
+
+
+        }
